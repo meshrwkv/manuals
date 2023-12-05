3ead80b74ae2ecbbd4e632af2f2eb287564f3bf4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-05 09:49:20 -0800

    Add polygen

--- graph_conv.gd
+++ graph_conv.gd
@@ -19,14 +19,32 @@ func get_mesh_data(mesh: Mesh) -> Dictionary:
     for i in range(tool.get_vertex_count()):
         vertices.append(tool.get_vertex(i))
 
+    # Sort vertices in z-y-x order [43] Charlie Nash, Yaroslav Ganin, SM Ali Eslami, and Peter Battaglia. Polygen
+    vertices.sort_custom(self, "compare_vertices")
+
     for i in range(tool.get_edge_count()):
         edges.append(tool.get_edge_vertices(i))
 
     for i in range(tool.get_face_count()):
         normals.append(tool.get_face_normal(i))
         areas.append(tool.get_face_area(i))
-        
-    return {"vertices": vertices, "edges": edges, "normals": normals, "areas": areas}
+
+    # Order faces based on their lowest vertex index
+    var faces: Array = []
+    for i in range(tool.get_face_count()):
+        var face_vertices: Array = tool.get_face_vertices(i)
+        face_vertices.sort()
+        faces.append(face_vertices)
+
+    return {"vertices": vertices, "edges": edges, "normals": normals, "areas": areas, "faces": faces}
+
+func compare_vertices(a: Vector3, b: Vector3) -> int:
+    if a.z != b.z:
+        return a.z < b.z ? -1 : 1
+    elif a.y != b.y:
+        return a.y < b.y ? -1 : 1
+    else:
+        return a.x < b.x ? -1 : 1
 
 func forward(data: Dictionary) -> Array:
     var x: Array = data["x"]
@@ -36,3 +54,10 @@ func forward(data: Dictionary) -> Array:
     x = conv.forward(x, edge_index)
     
     return x
+
+## Citations
+
+# [43] Charlie Nash, Yaroslav Ganin, SM Ali Eslami, and Peter
+# Battaglia. Polygen: An autoregressive generative model of
+# 3d meshes. In International conference on machine learning,
+# pages 7220–7229. PMLR, 2020
