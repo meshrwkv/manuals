e4add6d62658aaea9d3c55b64fa71be46e7048d8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-05 19:56:19 -0800

    "Only triangles or quads are supported"

--- face_data.gd
+++ face_data.gd
@@ -4,7 +4,10 @@ var normal: Vector3
 var area: float
 var angle: float
 
-func _init(_vertices: Array, _normal: Vector3, _area: float, _angle: float):
+func _init(_vertices: PackedVector3Array, _normal: Vector3, _area: float, _angle: float):
+    if not vertices.size() in [3, 4]:
+        print("Only triangles or quads are supported")
+        return
     vertices = _vertices
     normal = _normal
     area = _area
