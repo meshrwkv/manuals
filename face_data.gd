2cb7873fc80cd8e5a10d92010052dd752168a91a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-05 20:11:31 -0800

    Only triangles.

--- face_data.gd
+++ face_data.gd
@@ -6,24 +6,9 @@ var area: float
 var angle: float
 
 func _init(_vertices: PackedVector3Array, _normal: Vector3, _area: float, _angle: float) -> void:
-    if _vertices.size() > 4 or _vertices.size() < 0:
-        print("Only empty shapes, points, lines, triangles or quads are supported")
+    if _vertices.size() != 3:
+        print("Only triangles are supported")
         return
-    match _vertices.size():
-        0: # Empty shape
-            for i in range(4): # Add four null vertices
-                _vertices.append(Vector3())
-        1: # Point
-            _vertices.append(_vertices[0]) # Duplicate the point three times
-            _vertices.append(_vertices[0])
-            _vertices.append(_vertices[0])
-        2: # Line
-            _vertices.append(_vertices[0]) # Duplicate one of the points twice
-            _vertices.append(_vertices[0])
-        3: # Triangle
-            _vertices.append(_vertices[_vertices.size() - 1]) # Duplicate the last vertex
-        4: # Quad
-            pass # No need to change anything for quads
     vertices = _vertices
     normal = _normal
     area = _area
