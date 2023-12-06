b58ad76a60a0be86964b0d57e6af8eed1002d501
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-05 20:03:14 -0800

    Only support quads.

--- face_data.gd
+++ face_data.gd
@@ -1,13 +1,16 @@
 class_name FaceData
+
 var vertices: Array
 var normal: Vector3
 var area: float
 var angle: float
 
-func _init(_vertices: PackedVector3Array, _normal: Vector3, _area: float, _angle: float):
-    if not vertices.size() in [3, 4]:
+func _init(_vertices: PackedVector3Array, _normal: Vector3, _area: float, _angle: float) -> void:
+    if _vertices.size() not in [3, 4]:
         print("Only triangles or quads are supported")
         return
+    if _vertices.size() == 3:
+        _vertices.append(_vertices[_vertices.size() - 1])
     vertices = _vertices
     normal = _normal
     area = _area
