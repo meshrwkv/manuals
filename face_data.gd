6f0862edbf68eb83447fc1b162de4df7804adca7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-05 20:06:46 -0800

    Support degenerate quads.

--- face_data.gd
+++ face_data.gd
@@ -6,11 +6,21 @@ var area: float
 var angle: float
 
 func _init(_vertices: PackedVector3Array, _normal: Vector3, _area: float, _angle: float) -> void:
-    if _vertices.size() not in [3, 4]:
-        print("Only triangles or quads are supported")
+    if _vertices.size() > 4 or _vertices.size() < 1:
+        print("Only points, lines, triangles or quads are supported")
         return
-    if _vertices.size() == 3:
-        _vertices.append(_vertices[_vertices.size() - 1])
+    match _vertices.size():
+        1: # Point
+            _vertices.append(_vertices[0]) # Duplicate the point three times
+            _vertices.append(_vertices[0])
+            _vertices.append(_vertices[0])
+        2: # Line
+            _vertices.append(_vertices[0]) # Duplicate one of the points twice
+            _vertices.append(_vertices[0])
+        3: # Triangle
+            _vertices.append(_vertices[_vertices.size() - 1]) # Duplicate the last vertex
+        4: # Quad
+            pass # No need to change anything for quads
     vertices = _vertices
     normal = _normal
     area = _area
